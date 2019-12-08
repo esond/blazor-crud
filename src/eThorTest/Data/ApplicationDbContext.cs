035377172f072a223a341eee7a33a654f442b5d2
Eric Sondergard <4650644+esond@users.noreply.github.com>
2019-12-08 13:51:51 -0700

    Add test entity and related EF migration

--- src/eThorTest/Data/ApplicationDbContext.cs
+++ src/eThorTest/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace eThorTest.Data
 {
@@ -11,6 +9,19 @@ namespace eThorTest.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<EThorTestEntity> TestEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<EThorTestEntity>()
+                .Property(p => p.HardProperty)
+                .HasConversion(
+                    x => JsonConvert.SerializeObject(x),
+                    x => JsonConvert.DeserializeObject<string[]>(x));
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ src/eThorTest/Data/EThorTestEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eThorTest.Data
+{
+    public class EThorTestEntity : IComparable
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<string> HardProperty
+        {
+            get => _hardProperty ??= new List<string>();
+            set => _hardProperty = value;
+        }
+        private ICollection<string> _hardProperty;
+
+        #region Implementation of IComparable
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}

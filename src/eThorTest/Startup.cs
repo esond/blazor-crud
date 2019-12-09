2ad805cc8e4d9384b1f757672d7cedd0da095a09
Eric Sondergard <4650644+esond@users.noreply.github.com>
2019-12-08 19:07:15 -0700

    Configure EF to use an in-memory database

--- src/eThorTest/Startup.cs
+++ src/eThorTest/Startup.cs
@@ -33,8 +33,7 @@ namespace eThorTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseInMemoryDatabase("eThorTestDb"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();

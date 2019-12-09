a38af1a4559be727cb9f00910d4e210a12c651e7
Eric Sondergard <4650644+esond@users.noreply.github.com>
2019-12-08 19:57:05 -0700

    Extract a 'view' model for entities to centralize list <-> string conversion logic

---
+++ src/eThorTest/Models/EntityModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using eThorTest.Data;
+
+namespace eThorTest.Models
+{
+    public class EntityModel
+    {
+        private EThorTestEntity _entity;
+
+        public EntityModel()
+        {
+            _entity = new EThorTestEntity();
+        }
+
+        public EntityModel(EThorTestEntity entity)
+        {
+            _entity = entity;
+        }
+
+        public Guid Id => _entity.Id;
+
+        [Required]
+        public string Name
+        {
+            get => _entity.Name;
+            set => _entity.Name = value;
+        }
+
+        public IEnumerable<string> Properties
+        {
+            get => _entity.HardProperty;
+            set => _entity.HardProperty = value.ToList();
+        }
+
+        public string PropertiesValue
+        {
+            get => string.Join('\n', _entity.HardProperty);
+            set => _entity.HardProperty = value.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public EThorTestEntity GetEntity()
+        {
+            return _entity;
+        }
+    }
+}
